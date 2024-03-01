e9e3617de644a504c0d260b24db1764bf2152fe7
shreyas4510 <shreyasdeshmukh06@gmail.com>
2024-03-02 01:37:42 +0530

    feat: auth unit test cases backend

--- order-management-be/src/api/tests/modules/owner.test.js
+++ order-management-be/src/api/tests/modules/owner.test.js
@@ -1,93 +1,287 @@
 import ownerController from "../../controllers/owner.controllers";
 import { db } from "../../../config/database";
-import { invalidData, invalidDataMessage, invalidEmailMessage, invalidEmaildata, invalidPassMessage, invalidPassworddata, invalidPhoneMessage, invalidPhonedata, owner, ownerResponse } from "../utils/dummy.owner";
-import { STATUS_CODE } from "../../utils/common";
+import dummyData from "../utils/dummy.owner";
 
+// mock the database operations
 jest.mock('../../../config/database.js', () => ({
     db: {
         owners: {
-            create: jest.fn()
+            create: jest.fn(),
+            findOne: jest.fn()
         }
     }
 }));
 
-jest.mock('../../../config/email.js', () => ({
-    transporter: {
-        sendMail: jest.fn()
-    }
-}));
+// mock the email sending functions
+jest.mock('../../../config/email.js', () => ({ transporter: { sendMail: jest.fn() } }));
 
+// mocking console logs
+console.log = jest.fn();
+
+// mocking express res
 const res = {
     status: jest.fn().mockReturnThis(),
     send: jest.fn()
 };
 
 describe('testing owner cases', () => {
+    // Register owner test cases
     test('test invalid email to register a user', async () => {
-        await ownerController.create({ body: invalidEmaildata }, res);
+        const { invalidEmailData } = dummyData;
+         await ownerController.create({ body: invalidEmailData.body }, res);
 
         // Status code should be 400
-        expect(res.status).toHaveBeenCalledWith(STATUS_CODE.BAD_REQUEST);
+        expect(res.status).toHaveBeenCalledWith(invalidEmailData.res.code);
 
         // The res.send function is called exactly ones
         expect(res.send.mock.calls).toHaveLength(1);
 
-        const message = res.send.mock.calls[0][0].message;
-        expect(message).toEqual(invalidEmailMessage);
+        const data = res.send.mock.calls[0][0];
+        expect(data).toEqual(invalidEmailData.res.data);
     });
 
     test('test invalid password to register a user', async () => {
-        await ownerController.create({ body: invalidPassworddata }, res);
+        const { invalidPasswordData } = dummyData;
+        await ownerController.create({ body: invalidPasswordData.body }, res);
 
         // Status code should be 400
-        expect(res.status).toHaveBeenCalledWith(STATUS_CODE.BAD_REQUEST);
+        expect(res.status).toHaveBeenCalledWith(invalidPasswordData.res.code);
 
         // The res.send function is called exactly ones
         expect(res.send.mock.calls).toHaveLength(1);
 
-        const message = res.send.mock.calls[0][0].message;
-        expect(message).toContain(invalidPassMessage);
+        const data = res.send.mock.calls[0][0];
+        expect(data).toHaveProperty('message');
+        expect(data.message).toContain(invalidPasswordData.res.data.message);
     });
 
     test('test invalid phone number to register a user', async () => {
-        await ownerController.create({ body: invalidPhonedata }, res);
+        const { invalidPhoneData } = dummyData;
+        await ownerController.create({ body: invalidPhoneData.body }, res);
 
         // Status code should be 400
-        expect(res.status).toHaveBeenCalledWith(STATUS_CODE.BAD_REQUEST);
+        expect(res.status).toHaveBeenCalledWith(invalidPhoneData.res.code);
 
         // The res.send function is called exactly ones
         expect(res.send.mock.calls).toHaveLength(1);
 
-        const message = res.send.mock.calls[0][0].message;
-        expect(message).toEqual(invalidPhoneMessage);
+        const data = res.send.mock.calls[0][0];
+        expect(data).toEqual(invalidPhoneData.res.data);
     });
 
     test('test invalid body to register a user', async () => {
-        await ownerController.create({ body: invalidData }, res);
+        const { invalidData } = dummyData;
+        await ownerController.create({ body: invalidData.body }, res);
 
         // Status code should be 400
-        expect(res.status).toHaveBeenCalledWith(STATUS_CODE.BAD_REQUEST);
+        expect(res.status).toHaveBeenCalledWith(invalidData.res.code);
 
         // The res.send function is called exactly ones
         expect(res.send.mock.calls).toHaveLength(1);
 
-        const message = res.send.mock.calls[0][0].message;
-        expect(message).toEqual(invalidDataMessage);
+        const data = res.send.mock.calls[0][0];
+        expect(data).toEqual(invalidData.res.data);
     });
 
-    test('register an user', async () => {
+    test('test successful register of owner', async () => {
+        const { owner } = dummyData;
         // mock owner details
-        db.owners.create.mockResolvedValue(ownerResponse)
+        db.owners.create.mockResolvedValue(owner.db)
 
-        await ownerController.create({ body: owner }, res);
+        await ownerController.create({ body: owner.body }, res);
         
         // Status code should be 201
-        expect(res.status).toHaveBeenCalledWith(STATUS_CODE.CREATED);
+        expect(res.status).toHaveBeenCalledWith(owner.res.code);
 
         // The res.send function is called exactly ones
         expect(res.send.mock.calls).toHaveLength(1);
         
         // expected response should match the owner details from mocked database
-        expect(res.send).toHaveBeenCalledWith(ownerResponse);
+        expect(res.send).toHaveBeenCalledWith(owner.db);
     });
+
+    // Login owner test cases
+    test('test email not registered', async () => {        
+        const { unregisteredEmailData } = dummyData;
+
+        db.owners.findOne.mockResolvedValue(undefined);
+        await ownerController.login({ body: unregisteredEmailData.body }, res);
+
+        // Status code should be 404
+        expect(res.status).toHaveBeenCalledWith(unregisteredEmailData.res.code);
+
+        // The res.send function is called exactly ones
+        expect(res.send.mock.calls).toHaveLength(1);
+
+        // compare the error message
+        const data = res.send.mock.calls[0][0];
+        expect(data).toEqual(unregisteredEmailData.res.data);
+    })
+
+    test('test invalid password', async () => {
+        const { incorrectPasswordData } = dummyData;
+
+        db.owners.findOne.mockResolvedValue(incorrectPasswordData.db);
+        await ownerController.login({ body: incorrectPasswordData.body }, res);
+
+        // Status code should be 401
+        expect(res.status).toHaveBeenCalledWith(incorrectPasswordData.res.code);
+
+        // The res.send function is called exactly ones
+        expect(res.send.mock.calls).toHaveLength(1);
+
+        // compare the error message
+        const data = res.send.mock.calls[0][0];
+        expect(data).toHaveProperty('message');
+        expect(data.message).toEqual(incorrectPasswordData.res.data.message);
+
+    })
+
+    test('test email not verified', async () => {
+        const { inActiveData } = dummyData;
+
+        db.owners.findOne.mockResolvedValue(inActiveData.db);
+        await ownerController.login({ body: inActiveData.body }, res);
+
+        // Status code should be 401
+        expect(res.status).toHaveBeenCalledWith(inActiveData.res.code);
+
+        // The res.send function is called exactly ones
+        expect(res.send.mock.calls).toHaveLength(1);
+
+        // compare the error message
+        const data = res.send.mock.calls[0][0];
+        expect(data).toEqual(inActiveData.res.data);
+    })
+
+    test('test successful login of owner', async () => {
+        const { successLoginData } = dummyData;
+
+        db.owners.findOne.mockResolvedValue(successLoginData.db);
+        await ownerController.login({ body: successLoginData.body }, res);
+
+        // Status code should be 200
+        expect(res.status).toHaveBeenCalledWith(successLoginData.res.code);
+
+        // The res.send function is called exactly ones
+        expect(res.send.mock.calls).toHaveLength(1);
+
+        // compare the error message
+        const data = res.send.mock.calls[0][0];
+        expect(data).toHaveProperty('token');
+        expect(data).toHaveProperty('data');
+        expect(data.data).toEqual(successLoginData.db);
+
+    })
+
+    // verify test cases
+    test('test user already verified', async () => {
+        const { userAlreadyVerifiedData } = dummyData;
+
+        db.owners.findOne.mockResolvedValue(userAlreadyVerifiedData.db);
+        await ownerController.verify({ body: userAlreadyVerifiedData.body }, res);
+
+        // Status code should be 400
+        expect(res.status).toHaveBeenCalledWith(userAlreadyVerifiedData.res.code);
+
+        // The res.send function is called exactly ones
+        expect(res.send.mock.calls).toHaveLength(1);
+
+        // compare the error message
+        const data = res.send.mock.calls[0][0];
+        expect(data).toEqual(userAlreadyVerifiedData.res.data);
+    })
+
+    test('test expired link', async () => {
+        const { linkExpiredData } = dummyData;
+
+        db.owners.findOne.mockResolvedValue(linkExpiredData.db);
+        await ownerController.verify({ body: linkExpiredData.body }, res);
+
+        // Status code should be 410
+        expect(res.status).toHaveBeenCalledWith(linkExpiredData.res.code);
+
+        // The res.send function is called exactly ones
+        expect(res.send.mock.calls).toHaveLength(1);
+
+        // compare the error message
+        const data = res.send.mock.calls[0][0];
+        expect(data).toEqual(linkExpiredData.res.data);
+    })
+
+    test('test verify email', async () => {
+        const { verifyEmailData } = dummyData;
+
+        verifyEmailData.db.save = jest.fn();
+        db.owners.findOne.mockResolvedValue(verifyEmailData.db);
+        await ownerController.verify({ body: verifyEmailData.body }, res);
+
+        // Status code should be 200
+        expect(res.status).toHaveBeenCalledWith(verifyEmailData.res.code);
+
+        // The res.send function is called exactly ones
+        expect(res.send.mock.calls).toHaveLength(1);
+
+        // compare the error message
+        const data = res.send.mock.calls[0][0];
+        expect(data).toHaveProperty('token');
+        expect(data).toHaveProperty('data');
+        expect(data.data.status).toEqual(verifyEmailData.res.data.status);
+
+    })
+
+    // forgot password test cases
+    test('test not verified user', async () => {
+        const { unverifiedData } = dummyData;
+
+        db.owners.findOne.mockResolvedValue(unverifiedData.db);
+        await ownerController.forget({ body: unverifiedData.body }, res);
+
+        // Status code should be 403
+        expect(res.status).toHaveBeenCalledWith(unverifiedData.res.code);
+
+        // The res.send function is called exactly ones
+        expect(res.send.mock.calls).toHaveLength(1);
+
+        // compare the error message
+        const data = res.send.mock.calls[0][0];
+        expect(data).toEqual(unverifiedData.res.data);
+    })
+
+    test('test forgot password', async () => {
+        const { forgotPasswordData } = dummyData;
+
+        db.owners.findOne.mockResolvedValue(forgotPasswordData.db);
+        await ownerController.forget({ body: forgotPasswordData.body }, res);
+
+        // Status code should be 200
+        expect(res.status).toHaveBeenCalledWith(forgotPasswordData.res.code);
+
+        // The res.send function is called exactly ones
+        expect(res.send.mock.calls).toHaveLength(1);
+
+        // compare the error message
+        const data = res.send.mock.calls[0][0];
+        expect(data).toEqual(forgotPasswordData.res.data);
+    })
+
+    // reset password
+    test('test to reset password', async () => {
+        const { resetPasswordData } = dummyData;
+
+        resetPasswordData.db.save = jest.fn();
+        db.owners.findOne.mockResolvedValue(resetPasswordData.db);
+        await ownerController.reset({ body: resetPasswordData.body }, res);
+
+        // Status code should be 200
+        expect(res.status).toHaveBeenCalledWith(resetPasswordData.res.code);
+
+        // The res.send function is called exactly ones
+        expect(res.send.mock.calls).toHaveLength(1);
+
+        // compare the error message
+        const data = res.send.mock.calls[0][0];
+        expect(data).toEqual(resetPasswordData.res.data);
+        expect(resetPasswordData.db.password).toEqual(resetPasswordData.body.newPassword);
+    })
 });
--- order-management-be/src/api/tests/utils/dummy.owner.js
+++ order-management-be/src/api/tests/utils/dummy.owner.js
@@ -1,77 +1,263 @@
-export const invalidEmaildata = {
-    firstName: 'test',
-    lastName: 'test',
-    phoneNumber: 1234567890,
-    email: 'test$test.vi', // invalid email
-    password: 'U2FsdGVkX1/M/nm/UED5QUvoaIEZMa5p9y7ePJ0r8ik=',
-    addressLine1: 'test',
-    addressLine2: 'test',
-    city: 'test',
-    state: 'test',
-    zipCode: 123456,
-};
-export const invalidEmailMessage = '"email" must be a valid email';
+import moment from "moment";
+import { status } from "../../models/owner.model";
+import { STATUS_CODE } from "../../utils/common";
 
-export const invalidPassworddata = {
-    firstName: 'test',
-    lastName: 'test',
-    phoneNumber: 1234567890,
-    email: 'test@test.com',
-    password: 'U2FsdGVkX1+hsiMAdRRWOn8GAzrrRJSF/R4DTrRUxiM=', // inavlid password Test1234
-    addressLine1: 'test',
-    addressLine2: 'test',
-    city: 'test',
-    state: 'test',
-    zipCode: 123456,
-};
-export const invalidPassMessage = '"password" with value "Test1234" fails to match the required pattern';
-
-export const invalidPhonedata = {
-    firstName: 'test',
-    lastName: 'test',
-    phoneNumber: 123456789, // invalid phone with 9 digits 
-    email: 'test@test.com',
-    password: 'U2FsdGVkX1/M/nm/UED5QUvoaIEZMa5p9y7ePJ0r8ik=',
-    addressLine1: 'test',
-    addressLine2: 'test',
-    city: 'test',
-    state: 'test',
-    zipCode: 123456,
-};
-export const invalidPhoneMessage = '"phoneNumber" must be greater than or equal to 1000000000';
-
-export const invalidData = {
-    phoneNumber: 1234567890, 
-    email: 'test@test.com',
-    password: 'U2FsdGVkX1/M/nm/UED5QUvoaIEZMa5p9y7ePJ0r8ik=',
-    city: 'test',
-    state: 'test',
-    zipCode: 123456,
-};
-export const invalidDataMessage = '"firstName" is required';
-
-export const owner = {
-    firstName: 'test',
-    lastName: 'test',
-    phoneNumber: 1234567890,
-    email: 'test@test.com',
-    password: 'U2FsdGVkX1/M/nm/UED5QUvoaIEZMa5p9y7ePJ0r8ik=',
-    addressLine1: 'test',
-    addressLine2: 'test',
-    city: 'test',
-    state: 'test',
-    zipCode: 123456,
+const invalidEmailData = {
+    body: {
+        firstName: 'test',
+        lastName: 'test',
+        phoneNumber: 1234567890,
+        email: 'test$test.vi', // invalid email
+        password: 'U2FsdGVkX1/M/nm/UED5QUvoaIEZMa5p9y7ePJ0r8ik=',
+        addressLine1: 'test',
+        addressLine2: 'test',
+        city: 'test',
+        state: 'test',
+        zipCode: 123456,
+    },
+    res: {
+        code: STATUS_CODE.BAD_REQUEST,
+        data: { message: `"email" must be a valid email` }
+    }
 };
 
-export const ownerResponse = {
-    id: '60c688d6-5442-4569-9c8c-3f973b3ba554',
-    firstName: 'test',
-    lastName: 'test',
-    phoneNumber: 1234567890,
-    email: 'test@test.com',
-    addressLine1: 'test',
-    addressLine2: 'test',
-    city: 'test',
-    state: 'test',
-    zipCode: 123456,
+const invalidPasswordData = {
+    body: {
+        firstName: 'test',
+        lastName: 'test',
+        phoneNumber: 1234567890,
+        email: 'test@test.com',
+        password: 'U2FsdGVkX1+hsiMAdRRWOn8GAzrrRJSF/R4DTrRUxiM=', // inavlid password Test1234
+        addressLine1: 'test',
+        addressLine2: 'test',
+        city: 'test',
+        state: 'test',
+        zipCode: 123456,    
+    },
+    res : {
+        code: STATUS_CODE.BAD_REQUEST,
+        data: { message: '"password" with value "Test1234" fails to match the required pattern' }
+    }
 };
+
+const invalidPhoneData = {
+    body : {
+        firstName: 'test',
+        lastName: 'test',
+        phoneNumber: 123456789, // invalid phone with 9 digits 
+        email: 'test@test.com',
+        password: 'U2FsdGVkX1/M/nm/UED5QUvoaIEZMa5p9y7ePJ0r8ik=',
+        addressLine1: 'test',
+        addressLine2: 'test',
+        city: 'test',
+        state: 'test',
+        zipCode: 123456,
+    },
+    res: {
+        code: STATUS_CODE.BAD_REQUEST,
+        data: { message: '"phoneNumber" must be greater than or equal to 1000000000' }
+    }
+};
+
+const invalidData = {
+    body: {
+        phoneNumber: 1234567890, 
+        email: 'test@test.com',
+        password: 'U2FsdGVkX1/M/nm/UED5QUvoaIEZMa5p9y7ePJ0r8ik=',
+        city: 'test',
+        state: 'test',
+        zipCode: 123456,
+    },
+    res: {
+        code: STATUS_CODE.BAD_REQUEST,
+        data: { message: '"firstName" is required' }
+    }
+};
+
+const owner = {
+    body: {
+        firstName: 'test',
+        lastName: 'test',
+        phoneNumber: 1234567890,
+        email: 'test@test.com',
+        password: 'U2FsdGVkX1/M/nm/UED5QUvoaIEZMa5p9y7ePJ0r8ik=',
+        addressLine1: 'test',
+        addressLine2: 'test',
+        city: 'test',
+        state: 'test',
+        zipCode: 123456,
+    },
+    db: {
+        id: '60c688d6-5442-4569-9c8c-3f973b3ba554',
+        firstName: 'test',
+        lastName: 'test',
+        phoneNumber: 1234567890,
+        email: 'test@test.com',
+        addressLine1: 'test',
+        addressLine2: 'test',
+        city: 'test',
+        state: 'test',
+        zipCode: 123456,
+    },
+    res: {
+        code: STATUS_CODE.CREATED,
+    }
+};
+
+const unregisteredEmailData = {
+    body : {
+        email: 'unregistered-email@test.com',
+        password: 'U2FsdGVkX1/M/nm/UED5QUvoaIEZMa5p9y7ePJ0r8ik=' // Test@1234
+    },
+    res: {
+        code: STATUS_CODE.NOT_FOUND,
+        data: { message: 'Email not registered' }
+    }
+}
+
+const incorrectPasswordData = {
+    body: {
+        email: 'valid-email@test.com',
+        password: 'U2FsdGVkX1/M/nm/UED5QUvoaIEZMa5p9y7ePJ0r8ik=' // Test@1234
+    },
+    db: {
+        email: 'valid-email@test.com',
+        password: 'U2FsdGVkX1/AELUGLFLq8XnS9DAMLT0WCO36dja0CpA=' // Test@1237
+    },
+    res: {
+        code: STATUS_CODE.UNAUTHORIZED,
+        data: { message: 'Invalid password' }
+    }   
+}
+
+const inActiveData = {
+    body : {
+        email: 'valid-email@test.com',
+        password: 'U2FsdGVkX1/M/nm/UED5QUvoaIEZMa5p9y7ePJ0r8ik=' // Test@1234
+    },
+    db : {
+        email: 'valid-email@test.com',
+        password: 'U2FsdGVkX1/M/nm/UED5QUvoaIEZMa5p9y7ePJ0r8ik=', // Test@1234
+        status: status[1]
+    },
+    res: {
+        code: STATUS_CODE.FORBIDDEN,
+        data: { message: 'Email not verified' }
+    }
+}
+
+const successLoginData = {
+    body : {
+        email: 'valid-email@test.com',
+        password: 'U2FsdGVkX1/M/nm/UED5QUvoaIEZMa5p9y7ePJ0r8ik=' // Test@1234
+    },
+    db : {
+        email: 'valid-email@test.com',
+        password: 'U2FsdGVkX1/M/nm/UED5QUvoaIEZMa5p9y7ePJ0r8ik=', // Test@1234
+        status: status[0]
+    },
+    res: {
+        code: STATUS_CODE.OK,
+        data: { message: 'Email not verified' }
+    }
+}
+
+const userAlreadyVerifiedData = {
+    body: {
+        email: 'valid-test@test.com',
+    },
+    db: {
+        status: status[0]
+    },
+    res: {
+        code: STATUS_CODE.BAD_REQUEST,
+        data: { message: 'User already verified Please try login' }
+    }
+}
+
+const linkExpiredData = {
+    body: {
+        email: 'valid-test@test.com',
+        expires: moment().subtract(1, 'hour').valueOf()
+    },
+    db: {
+        status: status[1]
+    },
+    res: {
+        code: STATUS_CODE.GONE,
+        data: { message: `Sorry, the link has expired. We've sent a new one to your email. Please check and try again.` }
+    }
+}
+
+const verifyEmailData = {
+    body: {
+        email: 'valid-test@test.com',
+        expires: moment().add(1, 'hour').valueOf()
+    },
+    db: {
+        status: status[1]
+    },
+    res: {
+        code: STATUS_CODE.OK,
+        data: { status: status[0] }
+    }
+}
+
+const unverifiedData = {
+    body: {
+        email: 'valid-email@test.com'
+    },
+    db: {
+        status: status[1]
+    },
+    res: {
+        code: STATUS_CODE.FORBIDDEN,
+        data: { message: 'User has not verified email' }
+    }
+}
+
+const forgotPasswordData = {
+    body: {
+        email: 'valid-email@test.com'
+    },
+    db: {
+        status: status[0]
+    },
+    res: {
+        code: STATUS_CODE.OK,
+        data: { message: 'Recover password link sent. Please check your email.' }
+    }
+}
+
+const resetPasswordData = {
+    body: {
+        email: 'valid-email@test.com',
+        newPassword: 'U2FsdGVkX1/AELUGLFLq8XnS9DAMLT0WCO36dja0CpA=', // Test@1237
+        expires: moment().add(1, 'hour').valueOf()
+    },
+    db: {
+        password: ''
+    },
+    res: {
+        code: STATUS_CODE.OK,
+        data: { message: 'Password reset successfully' }
+    }
+}
+
+export default {
+    invalidEmailData,
+    invalidPasswordData,
+    invalidPhoneData,
+    invalidData,
+    owner,
+    unregisteredEmailData,
+    incorrectPasswordData,
+    inActiveData,
+    successLoginData,
+    userAlreadyVerifiedData,
+    linkExpiredData,
+    verifyEmailData,
+    unverifiedData,
+    forgotPasswordData,
+    resetPasswordData
+}
