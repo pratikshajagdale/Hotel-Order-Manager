f1feaf8594840a858bc4a9fdd0627b37a1b79b2a
shreyas4510 <shreyasdeshmukh06@gmail.com>
2024-03-13 19:33:10 +0530

    feat: swagger documentation added

--- order-management-be/src/config/express.js
+++ order-management-be/src/config/express.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../../swagger.json" with { type: "json" };
 import routes from "../api/routes/index.js";
 
 // create app using express
@@ -10,6 +12,9 @@ app.use(express.json());
 
 // allow all origins for cors
 app.use(cors({ origin: '*' }));
+
+// serve swaggger documentation
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // prefix for all the routes
 app.use('/api', routes);
