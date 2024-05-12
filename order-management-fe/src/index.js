439df1f793a772232ab234639dc751232e883f3d
shreyas4510 <shreyasdeshmukh06@gmail.com>
2024-05-12 18:16:40 +0530

    fix: minor bug fixed

--- order-management-fe/src/index.js
+++ order-management-fe/src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import store from './store/store';
+import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
